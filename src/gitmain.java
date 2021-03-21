0dcf054b8e7f25b37fc78f42f6edb32e6cd6b465
siddheshshankargithub <siddhesh.shankar@github.com>
2021-03-21 23:40:46 +0530

    Added hello world

--- src/gitmain.java
+++ src/gitmain.java
@@ -2,6 +2,7 @@ public class gitmain
 {
     public static void main(String [] args)
     {
-        // Code to be written here !!!
+        // Code to be written here !!!4
+        System.out.println("Hello world in Java is Beautiful !!");
     }
 }
